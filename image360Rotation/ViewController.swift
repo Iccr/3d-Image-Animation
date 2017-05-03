01ab93b99a91c55ee4298fffa838bb7035d0e7ca
shishir sapkota <shishirsapkota@shishirs-mac-mini.local>
2017-05-03 09:55:24 +0545

    extended outlets

--- image360Rotation/ViewController.swift
+++ image360Rotation/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnStartAnimation: UIButton!
+    @IBOutlet weak var imgAnimationView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
 
 }
 
